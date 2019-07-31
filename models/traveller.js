a347656ddf38bc8f59335740a0c141e105f7e879
bchutchison <benhutchison85@gmail.com>
2019-07-31 15:48:19 +0100

    total distance

--- models/traveller.js
+++ models/traveller.js
@@ -11,15 +11,21 @@ Traveller.prototype.getJourneyEndLocations = function () {
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  });
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter((journey) => {
+    return journey.distance > minDistance;
+  });
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce ((sum, journey) => {
+    return sum + journey.distance;
+  }, 0 );
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
